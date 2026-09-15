027dce5c0829d8359879356993f8a3d17d134955
Dave Cheez <davecheez21@gmail.com>
2026-09-15 19:19:46 +0100

    Add new host to ALLOWED_HOSTS in settings

--- base/settings.py
+++ base/settings.py
@@ -12,7 +12,7 @@ DEBUG = False
 #print("Debug: " , type(DEBUG), DEBUG)
 
 #print("DJS: ", SECRET_KEY)
-ALLOWED_HOSTS = ['phoenixvanz.com','https://coral-app-4w4xo.ondigitalocean.app/', '127.0.0.1', 'localhost', 'dev-phoenixvanz-wmmo3.ondigitalocean.app', 'dev.phoenixvanz.com']
+ALLOWED_HOSTS = ['phoenixvanz.com','https://coral-app-4w4xo.ondigitalocean.app/', '127.0.0.1', 'localhost', 'dev-phoenixvanz-wmmo3.ondigitalocean.app', 'dev.phoenixvanz.com', 'pheonix-production-591e.up.railway.app']
 
 # Application definition
 
